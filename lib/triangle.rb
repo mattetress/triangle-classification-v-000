042f617ebff1562d3e0738b8806ab3c077a961a1
Matt Etress <mattetress@me.com>
2018-10-11 06:44:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,19 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
 
-
-
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
 
-  
+  def kind
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene  
+    end
+
 end
