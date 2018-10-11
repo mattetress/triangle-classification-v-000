ec6437c30076c5cc40058bcbb09a2b0cebbda5c9
Matt Etress <mattetress@me.com>
2018-10-11 06:53:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,25 @@ class Triangle
   end
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
-      :equilateral
-    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-      :isosceles
-    else
-      :scalene
+    if valid?
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    else 
+      raise
+  end
+
+  def valid?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1 && side1 > 0 && side2 > 0 && side3 > 0
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Sides provided do not make a valid triangle."
     end
   end
 
